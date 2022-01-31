1ecc04869b6e024b80fbe1e4e72029df3017850a
Ilya <ilya.panshenskov@italki.com>
2022-01-31 16:45:05 +0800

    adding schedule task

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, session, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://sqwmtyaiabsiff:7ecca47616bef71798632076ef45a52d3fba58691acbda0766f04bdd87d0155e@ec2-3-222-49-168.compute-1.amazonaws.com:5432/dcr89i9f2cnh2l'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+db = SQLAlchemy(app)
+
+# Create our database model
+class TestTable(db.Model):
+    __tablename__ = "test_table"
+    listing_id = db.Column(db.Integer, primary_key=True)
+    added_time = db.Column(db.DateTime, unique=False)
+
+    def __init__(self, listing_id, added_time):
+        self.listing_id = listing_id
+        self.ticker = added_time
+
+    def __repr__(self):
+        return '<Listing Id %r>' % self.listing_id
+
 @app.route("/")
 def home():
+
+    db.create_all()
+
     return 'Hello!'
 
 if __name__  == '__main__': 
--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,19 @@
 import datetime
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import text
 
 if __name__ == "__main__":
-    date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    data = {}
-    data["text"] = "Hello World"
-    data["date_time"] = date_time
-    print(data)
+    cnx = create_engine('postgresql://sqwmtyaiabsiff:7ecca47616bef71798632076ef45a52d3fba58691acbda0766f04bdd87d0155e@ec2-3-222-49-168.compute-1.amazonaws.com:5432/dcr89i9f2cnh2l')
+
+    sql = '''select *
+             from public.test_table;'''
+
+    query = text(sql)
+    raw_data = pd.read_sql_query(query, cnx)
+
+    new_dict = {'listing_id': raw_data.tail(1).iloc[0][0],'added_date': datetime.datetime.now()}
+
+    raw_data = raw_data.append(new_dict, ignore_index = True)
+
+    raw_data.to_sql('test_table', cnx, schema = 'public', index = False, chunksize=100, if_exists='replace', method = 'multi')
