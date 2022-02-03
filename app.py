656ad499a18edcc563f7bd91cc07720495a8452a
Ilya <ilya.panshenskov@italki.com>
2022-02-03 12:56:07 +0800

    change url

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("POSTGRE")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SECRET_KEY'] = 'mysecretkey'
 
